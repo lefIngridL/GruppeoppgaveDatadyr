817a6067f5857a9d49dfc80cbd6f73afea7788b8
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-09-02 14:19:11 +0200

    innit innit
    Finally a working prototype! doggy!

--- script2.js
+++ script2.js
@@ -1,21 +1,14 @@
 // Model
-//const app = document.getElementById('app');
-// Model
 const room = document.getElementById('room');
 const body = document.body;
-//let TimerH = 0;
-//let TimerS = 0;
-//let TimerHu = 0;
+
 
 //const app = document.getElementById('app');
 var hungerOutPut='fornøyd';
 var sleepOutPut='uthvilt';
 var healthOutPut='Hygger meg';
-//var hungerOutPut2='fornøyd';
-//var sleepOutPut2='uthvilt';
-//var healthOutPut2='Hygger meg';
 
-let whichRoom = 'livingroom';
+
 
 // Datadyr bilder og rom bilder lagret i array
 const dogEmotions = [{
@@ -28,8 +21,9 @@ const dogEmotions = [{
     dogHungry: "https://media.discordapp.net/attachments/1135482561435541525/1146369318800592998/dogInterest-removebg-preview.png?width=442&height=473",
     dogCrying: "https://media.discordapp.net/attachments/1135482561435541525/1146369435490324581/dogCrying___Kopi-removebg-preview.png?width=405&height=473",
     dogInsult: "https://media.discordapp.net/attachments/1135482561435541525/1146369433850347540/dogInsult-removebg-preview.png?width=377&height=473",
+    dogDead: "https://cdn.discordapp.com/attachments/1135482561435541525/1147421234154450944/FAILUREskilt-removebg-preview.png"
 }];
-var dogMood = dogEmotions[0].dogAngry;
+
 const gameRooms = [{
     soveRomDag: 'url("https://cdn.discordapp.com/attachments/1135482561435541525/1146357216241258587/soveromLys.PNG")',
     soveRomNatt: 'url("https://cdn.discordapp.com/attachments/1135482561435541525/1146357217088507964/soveromNatt.PNG")',
@@ -39,16 +33,20 @@ const gameRooms = [{
     baderom: 'url("https://cdn.discordapp.com/attachments/1135482561435541525/1146357215746334770/modern-bathroom-furniture-interior-background-illustration-with-bathtub-shower-clean-up_2175-5218.avif")',
 }]
 
+var whichRoom;
+
 // View
+// i updateView ligger html innholdet
 updateView();
 function updateView(){
     
 body.style.backgroundImage = gameRooms[0].stueDag;
-console.log(whichRoom);
+
     html = /*html*/ `
     <div class="grid">
     <input type="button" class="btns" value="Start" onclick="trykk(this); statusbars()">
-            <input id="doggy" class="dataDyr" type="image" src="${dogMood}" alt="">
+            <input id="doggy" class="dataDyr" type="image" src="" alt="">
+            <div class="container">
             <div id="myProgressHunger">
                 <div id="hungerBar">100%</div>
                
@@ -60,10 +58,9 @@ console.log(whichRoom);
             <div id="myProgressSleep">
                 <div id="sleepBar">100%</div>
                 
+            </div> 
             </div>
-<div id="sleepBar2"></div>
-<div id="healthBar2"></div> 
-<div id="hungerBar2"></div>
+
 <br>
 <div class="arrowRight" id="arrowLeft" >⇐</div>
         <div class="arrowLeft" id="arrowRight">⇒</div>
@@ -84,6 +81,7 @@ console.log(whichRoom);
 const leftArrow = document.getElementById('arrowLeft');
 const rightArrow = document.getElementById('arrowRight');
 
+// funksjonene og eventlistener avgjør hvilket bakgrunnsbilde som dukker opp når du trykker på pilene
 leftArrow.addEventListener("click", updateArrowLeft);
 function updateArrowLeft() {
     if (whichRoom == 'livingroom') {
@@ -119,7 +117,7 @@ function updateArrowRight() {
 
     
 
-//updateViewLivingroom();
+//updateViewLivingroom(); bytter bakgrunnsbildet til stue
 function updateViewLivingroom() {
 body.style.backgroundImage = gameRooms[0].stueDag;
 whichRoom = 'livingroom';}
@@ -128,15 +126,17 @@ function updateViewBathroom() {
 whichRoom = 'bathroom';
 body.style.backgroundImage = gameRooms[0].baderom;}
 
-//updateViewKitchen();        
+//updateViewKitchen();  bytter bakgrunnsbildet til kjøkken       
 function updateViewKitchen() {
 whichRoom = 'kitchen';
 body.style.backgroundImage = gameRooms[0].kjøkken;}
-//updateViewBedroom();
+//updateViewBedroom(); bytter bakgrunnsbildet til soverom
 function updateViewBedroom() {
 whichRoom = 'bedroom';
 body.style.backgroundImage = gameRooms[0].soveRomDag;}
 
+
+//Funksjonen starter opp behovsmålerne
 function statusbars() {
     hungerBar();
      healthBar();
@@ -144,7 +144,7 @@ function statusbars() {
  }
  
  
- 
+//funksjonene hungerBar, healthBar og sleepBar sørger for at behovsbarene animeres 
    
    var i = 100;
    var id;
@@ -154,7 +154,7 @@ function statusbars() {
        i = 1;
        var elem = document.getElementById("hungerBar");
         width = 100;
-       id = setInterval(frame, 2000);
+       id = setInterval(frame, 5000);
        function frame() {
          if (width <= 0) {
            clearInterval(id);
@@ -163,13 +163,13 @@ function statusbars() {
            if (width == 70){
              elem.style.backgroundColor = 'yellow';
              hungerOutPut = 'Sulten';
-             dogMood = dogEmotions[0].dogInsult;
+             //dogMood = dogEmotions[0].dogInsult;
              //console.log(hungerOutPut);
              //console.log('gul');
          } else if (width == 30){
              elem.style.backgroundColor = 'red';
              hungerOutPut = 'Dør av sult';
-             dogMood = dogEmotions[0].dogCrying;
+             //dogMood = dogEmotions[0].dogCrying;
              //console.log(hungerOutPut);
              //console.log('rød');
          } else if (width == 100) {
@@ -177,7 +177,7 @@ function statusbars() {
              hungerOutPut = 'Fornøyd';
              //console.log(hungerOutPut);
              //console.log('grønn');
-             dogMood = dogEmotions[0].dogHappy;
+             //dogMood = dogEmotions[0].dogHappy;
          }
            width--;
            elem.style.width = width + "%";
@@ -197,7 +197,7 @@ function statusbars() {
        o = 1;
        var elem = document.getElementById("healthBar");
        width2 = 100;
-       id2 = setInterval(frame, 2000);
+       id2 = setInterval(frame, 5000);
        function frame() {
          if (width2 <= 0) {
            clearInterval(id2);
@@ -237,7 +237,7 @@ function statusbars() {
        j = 1;
        var elem = document.getElementById("sleepBar");
        width3 = 100;
-       id3 = setInterval(frame, 2000);
+       id3 = setInterval(frame, 5000);
        function frame() {
          if (width3 <= 0) {
            clearInterval(id3);
@@ -269,7 +269,7 @@ function statusbars() {
    
      }
    }
- 
+ // funksjonen trykk() sørger for å animere knapper (bytte ramme og tilbake)
    function trykk(tryktKnapp) {
      console.log(tryktKnapp);
      tryktKnapp.style.borderStyle = 'inset';
@@ -286,7 +286,7 @@ function statusbars() {
          healthBar();
 
      }
-     dogMood= dogEmotions[0].dogHappy;
+     const theWatcherTimer = setInterval(theWatcher, 2000); 
      const timer = setTimeout(reset, 200, tryktKnapp);
  
  
@@ -298,3 +298,86 @@ function statusbars() {
      
      
      }
+
+// Her kommer funksjonen theWatcher() som følger med på humør (maxMood, nedstammet fra bredden (width) på behovsbarene) og deretter setter det riktige bildet på plass    
+const myDoggy = document.querySelector('#doggy');
+function theWatcher() {
+  //console.log(i, o, j, width, width2, width3);
+  var maxMood = width+width2+width3;
+  //console.log(maxMood);
+  
+
+  if (maxMood >= 250) {
+    myDoggy.src = dogEmotions[0].dogHappy;
+    
+  }
+  else if (maxMood >= 200) {
+    myDoggy.src = dogEmotions[0].dogBored;
+  }
+  else if (maxMood >= 150) {
+    myDoggy.src = dogEmotions[0].dogInsult;
+  }
+  else if (maxMood >= 100) {
+    myDoggy.src = dogEmotions[0].dogSad;
+  }
+  else if (maxMood >0) {
+    myDoggy.src = dogEmotions[0].dogCrying;
+  }
+  else if (maxMood == 0) {
+    myDoggy.src = dogEmotions[0].dogDead;
+  }
+  else {
+    console.log("FAILURE");
+
+  }
+  return
+  
+ 
+}
+//disse konstantene må stå her, ellers krasjer programmet.. ellers skulle de vel vært i model..
+const arrowTimer = setInterval(buttonHider, 10);
+const watchRightArrow = document.querySelector('#arrowRight');
+const watchLeftArrow = document.querySelector('#arrowLeft');
+const sleepButton = document.querySelector('#sleep1');
+const foodButton = document.querySelector('#food1');
+const healthButton = document.querySelector('#health1');
+watchRightArrow.addEventListener("click", buttonHider());
+watchLeftArrow.addEventListener("click", buttonHider());
+// buttonHider gjør 2 ting, sørger for at knappene vises og skjules utifra hvilket rom vi er i, og bytter klasse på myDoggy, slik at plassering i rommet endres
+function buttonHider() {
+  if (body.style.backgroundImage==gameRooms[0].stueDag) {
+    whichRoom = 'livingroom';
+    //console.log(whichRoom);
+    //console.log(sleepButton);
+    sleepButton.style.display = 'none';
+    foodButton.style.display = 'none';
+    healthButton.style.display= '';
+    myDoggy.className = 'dataDyr';
+  }
+   else if (body.style.backgroundImage==gameRooms[0].soveRomDag) {
+    whichRoom = 'bedroom';
+    //console.log(whichRoom);
+    foodButton.style.display = 'none';
+    healthButton.style.display = 'none';
+    sleepButton.style.display = '';
+    myDoggy.className = 'dataDyrBedroom';
+
+  }
+  else if (body.style.backgroundImage==gameRooms[0].kjøkken ) {
+    whichRoom = 'kitchen';
+    //console.log(whichRoom);
+    sleepButton.style.display = 'none';
+    healthButton.style.display = 'none';
+    foodButton.style.display = '';
+    myDoggy.className = 'dataDyrKitchen';
+  }
+  else if (body.style.backgroundImage==gameRooms[0].baderom ) {
+    whichRoom = 'bathroom';
+    //console.log(whichRoom);
+    sleepButton.style.display = 'none';
+    healthButton.style.display = 'none';
+    foodButton.style.display = 'none';
+    myDoggy.className = 'dataDyrBathroom';
+  }
+  return;
+}
